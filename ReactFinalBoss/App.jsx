e1f5c2c6218546bc18a05fce2b003ce9e2ffe34f
Aman Kumar Goswami <goswamiamankumar24@gmail.com>
2026-09-07 19:12:18 +0530

    FinalBoss

---
+++ ReactFinalBoss/App.jsx
@@ -0,0 +1,13 @@
+export default function App(){
+    return (<h1>Hello Bruce Wayne! Your Gotham is beautiful.</h1>)
+};
+
+//Named Export
+export function Rohit(){
+    return <h2>Bruce's fear were Bats</h2>
+}
+
+
+
+
+//There can only be one Default Export
---
+++ ReactFinalBoss/first.jsx
@@ -0,0 +1,11 @@
+// To avoid all npm commands and all, here is the FinalBoss which does it all.FinalBoss
+
+// The command is:- npm create vite@latest
+
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import { Rohit } from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+    <App/>
+)
