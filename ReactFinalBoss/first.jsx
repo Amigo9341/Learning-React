5887c455f8dbc1433d6b57885614e81b9c9ffa68
Aman Kumar Goswami <goswamiamankumar24@gmail.com>
2026-09-07 20:35:13 +0530

    useStateHook

--- ReactFinalBoss/first.jsx
+++ ReactFinalBoss/first.jsx
@@ -4,12 +4,17 @@
 
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { Rohit } from './App.jsx'
-import { Mohit } from './App.jsx'
+import { Rohit, Mohit } from './App.jsx'
+
 
 createRoot(document.getElementById('root')).render(
+    <>
     <App/>
+    <Mohit></Mohit>
+    <Rohit></Rohit>
+    </>
 )
 
 //Here, App is the export default function. So, it is not written in curly {} braces.
 //Rohit and Mohit are named export functions and should always ne imported in Curly Braces {}.
+//Name of the export Function and the file name (here,"App.jsx") should be same.
