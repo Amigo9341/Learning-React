1472b77e23836b2c1fa83d3d29231e4d5364ff3c
Aman Kumar Goswami <goswamiamankumar24@gmail.com>
2026-09-09 18:31:28 +0530

    useEffect

--- useStateHook/vite-project/src/App.jsx
+++ useStateHook/vite-project/src/App.jsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
+
+//useState is a hook jo ki function ke jaisa maanlo. 
+//setCount v ek function ke jaisa hai jo count ka value by 1 increase krke count ko update krta hai and function ko re-render(dubara call) karta hai. 
+//Manually DOM Manipulation mai error aa jata hai. Watch lecture from 40th mminute.
+
 export default function App(){
 
-  let count = 0;
+  let [count, setCount] = useState(0);
+
+  // let count = 0;
+
+  // function increaseCount(){
+  //   count++;
+  //   const para = document.querySelector("p");       {/* This is DOM Manipulation and type of doing manual work*/}
+  //   para.textContent = `Counter : ${count}`;        {/* React should do this not us manually */}
+  // }
 
   function increaseCount(){
     count++;
-    const para = document.querySelector("p");
-    para.textContent = `Counter : ${count}`
+    setCount(count);             {/* const instead of let line5 mai likhne se count++ nhi krna hota and sirf setAman(count+1) se kaam ho jata*/}
   }
 
   return(
