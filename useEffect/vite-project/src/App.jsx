55a417f833bdb76089066a139b94f6b63bbbfe76
Aman Kumar Goswami <goswamiamankumar24@gmail.com>
2026-09-09 19:04:04 +0530

    hey

--- useEffect/vite-project/src/App.jsx
+++ useEffect/vite-project/src/App.jsx
@@ -8,8 +8,22 @@ function App() {
 
     const response = await fetch("https://api.github.com/users");
     const data = await response.json();
+    setUser(data);
   }
 
   githubProfile();
+
+  return(
+    <>
+    <h1>Github Users</h1>
+    <div style={{display:"flex", justifyContent:"center", alignItems:"center", flexWrap:"wrap"}}>
+      {
+        
+
+      }
+      </div>
+    
+    </>
+  )
 
 }export default App;
