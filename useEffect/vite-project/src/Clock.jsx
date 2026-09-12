991c1ccb2a8f83aa4d6ceb5563285a95e3fc879e
Aman Kumar Goswami <goswamiamankumar24@gmail.com>
2026-09-13 01:11:44 +0530

    dependancy

--- useEffect/vite-project/src/Clock.jsx
+++ useEffect/vite-project/src/Clock.jsx
@@ -3,17 +3,27 @@ import { useEffect, useState } from "react";
 function Clock(){
 
     const [time, setTime] = useState(new Date().toLocaleTimeString());
+    const [show, setShow] = useState(true);
 
     useEffect(() =>{
-    setInterval(() => {
+
+    if(!show)
+        return;
+
+    const interValid = setInterval(() => {
         setTime(new Date().toLocaleTimeString());
         console.log("Hii");         
     },1000)
-},[])
+
+    return () => {
+        clearInterval(interValid);
+    }
+},[show])
 
     return (
         <>
-        <h1>Current Time: {time}</h1>
+        <button onClick={()=>setShow(!show)}>{show?"hide":"show"}</button>
+        {show && <h1>Current Time : {time}</h1>}
         </>
     )
 
@@ -21,3 +31,5 @@ function Clock(){
 
 //setInterval will run like crazy. At first, it will be called once, then after 1 second it will run twice, next second thrice and on and on.
 //Its solution is "useEffect" Hook.
+//setInterval pehle jaisa to nahi but still chalta rahega, constantly badhega.
+//So we will use clearInterval for it to stop before the change in the dependency array.
