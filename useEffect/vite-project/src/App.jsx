72c7029233bfb0ec18a9fa25f6ba3015272f6b78
Aman Kumar Goswami <goswamiamankumar24@gmail.com>
2026-09-12 19:38:59 +0530

    clock

--- useEffect/vite-project/src/App.jsx
+++ useEffect/vite-project/src/App.jsx
@@ -1,21 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
 
   const [user, setUser] = useState([]);
+  //const [name, setName] = useState("");
+  const [count, setCount] = useState(30);
+
+  useEffect(() => {
 
   async function githubProfile(){
 
-    const response = await fetch("https://api.github.com/users");
+    const response = await fetch(`https://api.github.com/users?per_page=${count}`);
     const data = await response.json();
     setUser(data);
   }
 
   githubProfile();
+},[count])                   //This is the dependency array
+
+
+  // function handleChange(e) {
+  //   //console.log(e.target.value);
+  //   setName(e.target.value.toUppercase());
+  // }
+  
 
   return(
     <>
     <h1>Github Users</h1>
+    <input type="text" value={count} onChange={(e) => setCount(e.target.value)}></input>
     <div style={{display:"flex", justifyContent:"center", alignItems:"center", flexWrap:"wrap", gap:"20px"}}>
       {
         user.map((Aman) => (
@@ -35,3 +48,5 @@ function App() {
 //useEffect sbse late run karega. Sabke run krne ke baad.
 //Ye sirf render krne pe function call krega, Re-rendering pe nahi.
 //But there is a conditon. Blank '[]' rkhne pe hi kaam krega wrna nahi krega.
+//If the dependency array is empty, then the useEffect hook wont re render.
+//But if the dependency array is changed, it will re render.
---
+++ useEffect/vite-project/src/Clock.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+function Clock(){
+
+    const [time, setTime] = useState(new Date().toLocaleTimeString());
+
+    useEffect(() =>{
+    setInterval(() => {
+        setTime(new Date().toLocaleTimeString());
+        console.log("Hii");         
+    },1000)
+})
+
+    return (
+        <>
+        <h1>Current Time: {time}</h1>
+        </>
+    )
+
+}export default Clock;
+
+//setInterval will run like crazy. At first, it will be called once, then after 1 second it will run twice, next second thrice and on and on.
+//Its solution is "useEffect" Hook.
